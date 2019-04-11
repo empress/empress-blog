7b0dc8e4f91e35fabb2d0e5edf6d6b35dc4f5c1a
Chris Manson <chris@manson.ie>
2019-04-11 14:42:54 +0100

    change to using query when pagination is turned on in config

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,7 +8,18 @@ export default Controller.extend({
   blog: service(),
   url: service(),
 
+  queryParams: Object.freeze(['page']),
+  page: '/content/content-0.json',
+
   coverImageStyle: computed('blog.coverImage', function() {
     return htmlSafe(`background-image: url(${get(this, 'url.prefix')}${get(this, 'blog.coverImage')})`);
+  }),
+
+  pageNumber: computed('page', function() {
+    return this.page.match(/content\/content-(.*).json/)[1];
+  }),
+
+  numerOfPages: computed('model.links.last', function() {
+    return this.model.links.last.match(/content\/content-(.*).json/)[1];
   })
 })
