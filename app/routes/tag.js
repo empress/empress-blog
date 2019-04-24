1d38c4d164f72ed792b0efa4dad59a679462e1f4
Chris Manson <chris@manson.ie>
2019-04-24 10:14:38 +0100

    fix styling oddity on the tag page

--- app/routes/tag.js
+++ app/routes/tag.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  classNames: ["tag-template"],
+
   model(params) {
     // load content first for ember-data autopopulation
     return this.store.findAll('content').then(() => {
