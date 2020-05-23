92b18b9cd7b9bc188c27d52b07e8a5ff7e7b5867
Chris Manson <chris@manson.ie>
2020-05-23 16:18:28 +0100

    make sure included posts on tags are ordered by date decending

--- node-tests/tag-include-posts.js
+++ node-tests/tag-include-posts.js
@@ -66,5 +66,46 @@ posts:
 `)
   })
 
-  it('should order the relationship ids for tags ordered by date decending');
+  it('should order the relationship ids for tags ordered by date decending', async function() {
+    const files = await buildFiles({
+      content: {
+        'a-post-most-recent.md': `---
+date: 2020-01-10
+tags:
+- new
+---
+# Hello world A`,
+        'b-post-oldest.md': `---
+date: 2010-01-10
+tags:
+- new
+---
+# Hello world B`,
+        'c-post-medium-recent.md': `---
+date: 2020-01-05
+tags:
+- new
+---
+# Hello world C`},
+      tag: {
+        'new.md': `---
+name: New Posts
+image: ''
+imageMeta: ''
+---
+`}
+    });
+
+    expect(files).to.have.property('new.md');
+    expect(files['new.md']).to.equal(`---
+name: New Posts
+image: ''
+imageMeta: ''
+posts:
+  - a-post-most-recent
+  - c-post-medium-recent
+  - b-post-oldest
+---
+`)
+  });
 })
