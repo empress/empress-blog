68de8c5bc3d03b34eb5498195790811d95ace0e4
Chris Manson <chris@manson.ie>
2020-07-13 11:59:11 +0100

    use empress-blueprint-helpers for config writing

--- blueprints/empress-blog/index.js
+++ blueprints/empress-blog/index.js
@@ -1,8 +1,7 @@
 /* eslint-env node */
-const recast = require('recast');
-const { readFileSync, writeFileSync } = require('fs');
 const path = require('path');
 const stringUtils = require('ember-cli-string-utils');
+const { applyBuildConfig, applyConfig } = require('empress-blueprint-helpers');
 
 module.exports = {
   description: 'The default blueprint for empress-blog.',
@@ -36,8 +35,7 @@ module.exports = {
     };
   },
 
-  afterInstall() {
-
+  async afterInstall() {
     let devInstall = {
       packages: [
         'prember',
@@ -58,110 +56,23 @@ module.exports = {
       };
     }
 
-    return this.addAddonsToProject(install).then(() => {
-      return this.addAddonsToProject(devInstall);
-    }).then(() => {
-      const builders = recast.types.builders;
+    await this.addAddonsToProject(install);
+    await this.addAddonsToProject(devInstall);
 
-      const code = readFileSync('./ember-cli-build.js');
-      const ast = recast.parse(code);
+    applyBuildConfig('fingerprint', { extensions: ['js', 'css', 'map'] }, true);
 
-      recast.visit(ast, {
-        visitNewExpression: function (path) {
-          var node = path.node;
-
-          if (node.callee.name === 'EmberApp'
-              || node.callee.name === 'EmberAddon') {
-            // console.log(node, node.arguments)
-            const configNode = node.arguments.find(element => element.type === 'ObjectExpression');
-
-            let fingerprint = configNode.properties.find(property => property.key.value === 'fingerprint');
-
-            if(!fingerprint) {
-              fingerprint = builders.property(
-                'init',
-                builders.identifier('fingerprint'),
-                builders.objectExpression([])
-              )
-              configNode.properties.push(fingerprint);
-            }
-
-            // remove extensions from
-            const properties = fingerprint.value.properties.filter(property => property.key.value !== 'extensions');
-
-            properties.push(recast.types.builders.property(
-              'init',
-              builders.identifier('extensions'),
-              builders.arrayPattern([
-                builders.literal('js'),
-                builders.literal('css'),
-                builders.literal('map'),
-              ])
-            ));
-
-            fingerprint.value.properties = properties;
-
-
-            // console.log(fingerprint);
-            return false;
-          } else {
-            this.traverse(path);
-          }
-        }
-      });
-
-      writeFileSync('./ember-cli-build.js', recast.print(ast, { tabWidth: 2, quote: 'single' }).code);
-
-      let configFile = './config/environment.js'
-
-      if(this.project.isEmberCLIAddon()) {
-        configFile = './tests/dummy/config/environment.js';
-      }
-
-      const config = readFileSync(configFile);
-      const configAst = recast.parse(config);
-
-      recast.visit(configAst, {
-        visitVariableDeclaration: function (path) {
-          var node = path.node;
-
-          const env = node.declarations.find(declaration => declaration.id.name === 'ENV');
-
-          if (env) {
-            let blog = env.init.properties.find(property => property.key.value === 'blog');
-
-            if(!blog) {
-              blog = builders.property(
-                'init',
-                builders.identifier('blog'),
-                builders.objectExpression([
-                  builders.property('init', builders.identifier('title'), builders.literal('empress-blog')),
-                  builders.property('init', builders.identifier('description'), builders.literal('Static Blog System - Built with Ember')),
-                  builders.property('init', builders.identifier('coverImage'), builders.literal('/images/blog-cover.jpg')),
-                  builders.property('init', builders.identifier('navigation'), builders.arrayPattern([
-                    builders.objectExpression([
-                      builders.property('init', builders.identifier('label'), builders.literal('Home')),
-                      builders.property('init', builders.identifier('route'), builders.literal('index')),
-                    ]),
-                    builders.objectExpression([
-                      builders.property('init', builders.identifier('label'), builders.literal('Built by Chris Manson')),
-                      builders.property('init', builders.identifier('route'), builders.literal('page')),
-                      builders.property('init', builders.identifier('id'), builders.literal('chris-manson')),
-                    ])
-                  ])),
-                ])
-              )
-              env.init.properties.push(blog);
-            }
-
-            return false;
-          }
-
-          this.traverse(path);
-        }
-      });
-
-      writeFileSync(configFile, recast.print(configAst, { tabWidth: 2, quote: 'single' }).code);
+    applyConfig(this.project, 'blog', {
+      title: 'empress-blog',
+      description: 'Static Blog System - Built with Ember',
+      coverImage: '/images/blog-cover.jpg',
+      navigation: [{
+        label: 'Home',
+        route: 'index'
+      }, {
+        label: 'Built by Chris Manson',
+        route: 'page',
+        id: 'chris-manson'
+      }]
     });
   },
 
