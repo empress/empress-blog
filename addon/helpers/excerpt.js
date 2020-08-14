3f068e1c651ba13d8d236eb695a28c8beb470eef
nickschot <nickschot@gmail.com>
2020-08-14 21:59:36 +0200

    replace lodash usage with native or ember alternatives

--- addon/helpers/excerpt.js
+++ addon/helpers/excerpt.js
@@ -1,45 +1,44 @@
 import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
-import pick from 'lodash/pick';
-import keys from 'lodash/keys';
-import isEmpty from 'lodash/isEmpty';
+import { isEmpty } from '@ember/utils';
 import downsize from 'downsize-cjs';
 
 export function getExcerpt(html, truncateOptions) {
-    truncateOptions = truncateOptions || {};
-    // Strip inline and bottom footnotes
-    var excerpt = html.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
-    excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
-    // Strip other html
-    excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
-    excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
+  truncateOptions = truncateOptions || {};
+  // Strip inline and bottom footnotes
+  var excerpt = html.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
+  excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
+  // Strip other html
+  excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
+  excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
 
-    if (!truncateOptions.words && !truncateOptions.characters) {
-        truncateOptions.words = 50;
-    }
+  if (!truncateOptions.words && !truncateOptions.characters) {
+    truncateOptions.words = 50;
+  }
 
-    return downsize(excerpt, truncateOptions);
+  return downsize(excerpt, truncateOptions);
 }
 
 export function excerpt(content, options/*, hash*/) {
   var truncateOptions = options || {};
   var excerptText = options.custom_excerpt ? String(options.custom_excerpt) : String(content);
 
-    truncateOptions = pick(truncateOptions, ['words', 'characters']);
-    keys(truncateOptions).map(function (key) {
-        truncateOptions[key] = parseInt(truncateOptions[key], 10);
-    });
+  let { words, characters } = truncateOptions;
+  truncateOptions = { words, characters };
+  Object.keys(truncateOptions).map(function (key) {
+    truncateOptions[key] = parseInt(truncateOptions[key], 10);
+  });
 
-    if (!isEmpty(options.custom_excerpt)) {
-        truncateOptions.characters = options.custom_excerpt.length;
-        if (truncateOptions.words) {
-            delete truncateOptions.words;
-        }
+  if (!isEmpty(options.custom_excerpt)) {
+    truncateOptions.characters = options.custom_excerpt.length;
+    if (truncateOptions.words) {
+      delete truncateOptions.words;
     }
+  }
 
-    return htmlSafe(
-        getExcerpt(excerptText, truncateOptions)
-    );
+  return htmlSafe(
+    getExcerpt(excerptText, truncateOptions)
+  );
 }
 
 export default helper(excerpt);
