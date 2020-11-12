51e72d3f0738889f1ea5aa01acdb0d88c4ec044d
nickschot <nickschot@gmail.com>
2020-11-12 11:34:50 +0100

    fix: update Tag serializer to use _postIds instead of postIds

--- app/serializers/tag.js
+++ app/serializers/tag.js
@@ -3,7 +3,7 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
   normalize() {
     let tag = this._super(...arguments);
-    tag.data.attributes.postIds = tag.data.relationships.posts?.data.map(p => p.id) ?? [];
+    tag.data.attributes._postIds = tag.data.relationships.posts?.data.map(p => p.id) ?? [];
     return tag;
   }
 });
