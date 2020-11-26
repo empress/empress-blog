69623202a66edf23adda70b0b133ed909f3fc57e
Chris Manson <chris@manson.ie>
2020-11-26 18:04:59 +0000

    add tests for meta data

---
+++ tests/acceptance/meta-test.js
@@ -0,0 +1,42 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | meta test', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('index meta', async function(assert) {
+    await visit('/');
+
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'empress-blog: The simple JAM stack blog');
+  });
+
+  test('content meta', async function(assert) {
+    await visit('/welcome');
+
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Welcome to empress-blog');
+  })
+
+  test('page meta', async function(assert) {
+    await visit('/page/chris-manson');
+
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Built with ❤️ by Chris Manson');
+  });
+
+  test('author meta', async function(assert) {
+    await visit('/author/ghost');
+
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Author: Ghost');
+  });
+
+  test('tag meta', async function(assert) {
+    await visit('/tag/getting-started');
+
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Tag: Getting Started');
+  });
+});
--- tests/acceptance/smoke-test.js
+++ tests/acceptance/smoke-test.js
@@ -8,19 +8,24 @@ module('Acceptance | smoke test', function(hooks) {
   test('navigate to all routes', async function(assert) {
     await visit('/');
     assert.equal(currentURL(), '/');
+    assert.dom('head title', document).hasText('empress-blog: The simple JAM stack blog');
 
     await click('.post-card-content-link');
     assert.equal(currentURL(), '/welcome');
+    assert.dom('head title', document).hasText('Welcome to empress-blog');
 
     await click('.nav .nav-Built a');
     assert.equal(currentURL(), '/page/chris-manson');
+    assert.dom('head title', document).hasText('Built with ❤️ by Chris Manson');
 
     await click('.nav .nav-Home a');
     await click('.static-avatar');
     assert.equal(currentURL(), '/author/ghost');
+    assert.dom('head title', document).hasText('Author: Ghost');
 
     await click('.post-card-image-link');
     await click('.post-full-header a');
     assert.equal(currentURL(), '/tag/getting-started');
+    assert.dom('head title', document).hasText('Tag: Getting Started');
   });
 });
