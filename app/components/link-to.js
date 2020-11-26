ff5e0a4bd97b761ebfd07ecc47c3bc9876416650
nickschot <nickschot@gmail.com>
2020-11-26 15:22:09 +0100

    replace naive scroll to top implementation with ember-scroll

--- app/components/link-to.js
+++
@@ -1,9 +0,0 @@
-import LinkComponent from '@ember/routing/link-component';
-
-export default LinkComponent.extend({
-  click() {
-    if(window.scrollTo) {
-      window.scrollTo(0,0);
-    }
-  }
-});
