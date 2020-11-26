f0ef9018ef96c20c45bfb4c5390b74b091ac6dc5
Chris Manson <chris@manson.ie>
2020-11-26 20:39:59 +0000

    add meta test for discription

--- tests/acceptance/meta-test.js
+++ tests/acceptance/meta-test.js
@@ -10,6 +10,8 @@ module('Acceptance | meta test', function(hooks) {
 
     assert.dom('head meta[property="og:title"]', document)
       .hasAttribute('content', 'empress-blog: The simple JAM stack blog');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', 'Thoughts, stories and ideas.');
   });
 
   test('content meta', async function(assert) {
@@ -17,6 +19,8 @@ module('Acceptance | meta test', function(hooks) {
 
     assert.dom('head meta[property="og:title"]', document)
       .hasAttribute('content', 'Welcome to empress-blog');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', `Hey! Welcome to empress-blog, it's great to have you :) We know that first impressions are important, so we've populated your new site with some initial Getting Started posts that will...`);
     assert.dom('head meta[name="twitter:label1"]', document)
       .hasAttribute('content', 'Written by');
     assert.dom('head meta[name="twitter:data1"]', document)
@@ -28,6 +32,8 @@ module('Acceptance | meta test', function(hooks) {
 
     assert.dom('head meta[property="og:title"]', document)
       .hasAttribute('content', 'Built with ❤️ by Chris Manson');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', `Chris Manson is a Ember enthusiast and a member of the Ember Core Learning Team. empress-blog was an experimental project that came out of the work on converting the Ember Guides to...`);
     assert.dom('head meta[name="twitter:label1"]', document)
       .hasAttribute('content', 'Written by');
     assert.dom('head meta[name="twitter:data1"]', document)
@@ -39,6 +45,8 @@ module('Acceptance | meta test', function(hooks) {
 
     assert.dom('head meta[property="og:title"]', document)
       .hasAttribute('content', 'Author: Ghost');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', 'You can delete this user to remove all the welcome posts');
   });
 
   test('tag meta', async function(assert) {
@@ -46,5 +54,7 @@ module('Acceptance | meta test', function(hooks) {
 
     assert.dom('head meta[property="og:title"]', document)
       .hasAttribute('content', 'Tag: Getting Started');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', 'A description for the getting-started tag. If you delete this line it will say A collection of X posts where the description should go.');
   });
 });
