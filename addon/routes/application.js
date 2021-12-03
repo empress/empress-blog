431c4403083b3453d4b888538b0090f28c8c8fcc
Chris Manson <chris@manson.ie>
2021-12-03 10:32:12 +0000

    fix store injection deprecation

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,8 +1,11 @@
 /* eslint-disable ember/no-classic-classes */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import RSVP from 'rsvp';
 
 export default Route.extend({
+  store: service(),
+
   model() {
     return RSVP.hash({
       authors: this.store.findAll('author'),
