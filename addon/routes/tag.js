3f37b48efc85c9fa5c65ef7d5666b8d9c5a6c49e
Chris Manson <chris@manson.ie>
2021-12-03 10:32:12 +0000

    fix store injection deprecation

--- addon/routes/tag.js
+++ addon/routes/tag.js
@@ -1,8 +1,11 @@
 /* eslint-disable ember/no-classic-classes, prettier/prettier */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
   classNames: ["tag-template"],
+
+  store: service(),
 
   model(params) {
     // load content first for ember-data autopopulation
