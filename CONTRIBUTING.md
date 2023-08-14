398d9c1e7636b94b9172d18e9801a7f28a916a53
Chris Manson <chris@manson.ie>
2023-08-14 20:47:23 +0100

    reset to 3.28 with ember-cli-update

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,7 +3,7 @@
 ## Installation
 
 * `git clone <repository-url>`
-* `cd my-addon`
+* `cd empress-blog`
 * `npm install`
 
 ## Linting
--- tests/dummy/app/components/markdown-to-html.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import MarkdownToHTML from 'ember-cli-showdown/components/markdown-to-html';
-
-// I don't know why it's not picking up this from the app folder in the addon :shrug:
-export default MarkdownToHTML.extend({
-  classNames: ['kg-card-markdown'],
-})
