84a76172bd07b0b549d0f3a5ba308a69653f4294
Chris Manson <chris@manson.ie>
2025-10-18 22:42:53 +0100

    Update to 5.12 with ember-cli-update

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -38,6 +38,33 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+            'ember-data': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.4.0',
+            'ember-data': '~5.3.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-5.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.8.0',
+            'ember-data': '~5.3.0',
+          },
+        },
+      },
+      {
         name: 'ember-data-latest',
         npm: {
           devDependencies: {
