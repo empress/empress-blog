1f80a64431dfb6a855392c1e279401713e795521
Chris Manson <chris@manson.ie>
2025-10-23 10:26:34 +0100

    drop ember-cli-showdown dependency

--- app/components/markdown-to-html.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-import MarkdownToHTML from 'ember-cli-showdown/components/markdown-to-html';
-
-export default MarkdownToHTML.extend({
-  classNames: ['kg-card-markdown'],
-})
